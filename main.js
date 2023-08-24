17d46f7a48f6f25864b94d478aebec7277358e5b
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-08-24 17:32:03 +0100

    rework to fix bug on generating grids
    also on getting styles from elements

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const gridInput = document.querySelector("input");
 const gridLayoutDisplay = document.querySelector(".current-grid-layout");
 const generateGridLayoutBtn = document.querySelector(".generate-new-layout");
+const sketchPad = document.querySelector(".sketch-pad");
 
 generateGridLayoutBtn.addEventListener("click", () => {
     let value = gridInput.value;
@@ -14,4 +15,20 @@ gridInput.addEventListener("input", (e) => {
 
 function generateGrids(gridsCount = 16) {
     console.log(gridsCount);
+    console.log(sketchPad.style.width);
+    // const width = sketchPad.style.width = "550px";
+    // const height = sketchPad.style.height = "550px";
+
+    // const gridWidth = width / gridsCount;
+    // const gridHeight = height / gridsCount;
+
+    // sketchPad.textContent = "";
+
+    // for (let i = 0; i < gridsCount * gridsCount; i++) {
+    //     const grid = document.createElement("div");
+    //     grid.classList.add("grid");
+    //     grid.style.height = gridHeight + "px";
+    //     grid.style.width = gridWidth + "px";
+    //     sketchPad.appendChild(grid);
+    // }
 }
