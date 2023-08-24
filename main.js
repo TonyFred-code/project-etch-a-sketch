d09ad688a246d399d69e9f5d545d3bd976ad20b5
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-08-24 18:13:12 +0100

    Work on saving pad status (settings)

--- main.js
+++ main.js
@@ -2,11 +2,33 @@ const gridInput = document.querySelector("input");
 const gridLayoutDisplay = document.querySelector(".current-grid-layout");
 const generateGridLayoutBtn = document.querySelector(".generate-new-layout");
 const sketchPad = document.querySelector(".sketch-pad");
+const colorDisplay = document.querySelector(".color-display")
+const clearSketchPad = document.querySelector(".clear-sketch-pad");
+
+
+
+
+let displayedGrids = 0;
+let penColor = "black";
+
+clearSketchPad.addEventListener("click", () => {
+  generateGrids(displayedGrids)
+});
 
 generateGridLayoutBtn.addEventListener("click", () => {
     let value = gridInput.value;
     generateGrids(value);
 });
+
+const defaultColorChoices = document.querySelectorAll(".colors");
+
+defaultColorChoices.forEach((colorBox) => {
+  if (colorBox === defaultColorChoices[0]) {
+    colorBox.style.backgroundColor = "black";
+    return;
+  }
+  colorBox.style.backgroundColor = getRndRGB();
+})
 
 gridInput.addEventListener("input", (e) => {
   let value = e.target.value;
@@ -14,21 +36,43 @@ gridInput.addEventListener("input", (e) => {
 });
 
 function generateGrids(gridsCount = 16) {
-    console.log(gridsCount);
-    console.log(sketchPad.style.width);
-    // const width = sketchPad.style.width = "550px";
-    // const height = sketchPad.style.height = "550px";
+    const styles = window.getComputedStyle(sketchPad);
+    const width = styles.width.replace("px", "");
+    const height = styles.width.replace("px", "");
 
-    // const gridWidth = width / gridsCount;
-    // const gridHeight = height / gridsCount;
+    const gridWidth = width / gridsCount;
+    const gridHeight = height / gridsCount;
 
-    // sketchPad.textContent = "";
+    sketchPad.textContent = "";
 
-    // for (let i = 0; i < gridsCount * gridsCount; i++) {
-    //     const grid = document.createElement("div");
-    //     grid.classList.add("grid");
-    //     grid.style.height = gridHeight + "px";
-    //     grid.style.width = gridWidth + "px";
-    //     sketchPad.appendChild(grid);
-    // }
+    for (let i = 0; i < gridsCount * gridsCount; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        grid.style.height = gridHeight + "px";
+        grid.style.width = gridWidth + "px";
+        sketchPad.appendChild(grid);
+        grid.addEventListener("mouseover", changeGridBackgroundColor);
+    }
+
+    displayedGrids = gridsCount;
 }
+
+function generateRndInt(max, min) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRndRGB() {
+  return `rgb(${generateRndInt(255, 0)}, ${generateRndInt(255, 0)}, ${generateRndInt(255, 0)})`;
+}
+
+function changeGridBackgroundColor() {
+  let backgroundColor = penColor;
+  colorDisplay.style.backgroundColor = backgroundColor;
+  this.style.backgroundColor = backgroundColor;
+}
+
+generateGrids();
+
+// todo: work on generating grids
+// todo: think on mousedown functionality
+// todo: add a settings object to keep track of pad configs
